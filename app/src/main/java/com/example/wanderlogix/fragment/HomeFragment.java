58ed115183eb38bde1f22f8cba6fda2a359ad369
ChienWang <1039094655@qq.com>
2023-04-26 21:20:25 +0800

    first

---
+++ app/src/main/java/com/example/wanderlogix/fragment/HomeFragment.java
@@ -0,0 +1,4 @@
+package com.example.wanderlogix.fragment;
+
+public class HomeFragment {
+}
---
+++ app/src/main/java/com/example/wanderlogix/logix/LogixHelper.java
@@ -0,0 +1,5 @@
+package com.example.wanderlogix.logix;
+
+public class LogixHelper {
+    public static string getLogix
+}
