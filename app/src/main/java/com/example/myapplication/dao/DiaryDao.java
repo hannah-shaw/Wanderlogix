cc38ea0d55624ae213ed8543a90d380297a7e807
hannah shaw <xiaohannwork@163.com>
2023-05-01 11:05:04 +0800

    feat: diary dao

---
+++ app/src/main/java/com/example/myapplication/dao/DiaryDao.java
@@ -0,0 +1,39 @@
+package com.example.myapplication.dao;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.*;
+import com.example.myapplication.entity.DiaryEntry;
+import java.util.*;
+
+public interface DiaryDao {
+    @Query("SELECT * FROM diary_entries")
+    LiveData<List<DiaryEntry>> getAll();
+
+    @Query("SELECT * FROM diary_entries WHERE id = :diaryid")
+    DiaryEntry findByID(int diaryid);
+
+    @Insert
+    void insert(DiaryEntry diary);
+
+    @Delete
+    void delete(DiaryEntry diary);
+
+    @Update
+    void updateDiary(DiaryEntry diary);
+
+    @Query("DELETE FROM diary_entries")
+    void deleteAll();
+
+    @Query("SELECT fee FROM diary_entries")
+    List<Integer> getFee();
+
+    @Query("SELECT rating FROM diary_entries")
+    List<Integer> getRating();
+
+    @Query("SELECT location FROM diary_entries")
+    List<String> getLocation();
+
+    @Query("SELECT id FROM diary_entries")
+    List<Integer> getId();
+
+}
