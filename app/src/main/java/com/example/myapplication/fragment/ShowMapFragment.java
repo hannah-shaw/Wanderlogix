87270170f53e575776f59c584d82fdd092f3a542
Yliu <1223843119@qq.com>
2023-05-01 21:31:50 +0800

    增加了定位功能，实现了ROOM数据库连接，线程还需要优化

--- app/src/main/java/com/example/myapplication/fragment/ShowMapFragment.java
+++ app/src/main/java/com/example/myapplication/fragment/ShowMapFragment.java
@@ -9,86 +9,46 @@ import android.Manifest;
 import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
+import android.location.Location;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.myapplication.R;
 import com.example.myapplication.SelectMapActivity;
+import com.example.myapplication.database.DiaryDatabase;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
 
 import java.util.List;
 import java.util.Locale;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 public class ShowMapFragment extends Fragment {
-    private String location;
-    private OnMapReadyCallback callback = new OnMapReadyCallback() {
+    private String sLocation;
+    SupportMapFragment smf;
+    FusedLocationProviderClient client;
 
-        /**
-         * Manipulates the map once available.
-         * This callback is triggered when the map is ready to be used.
-         * This is where we can add markers or lines, add listeners or move the camera.
-         * In this case, we just add a marker near Sydney, Australia.
-         * If Google Play services is not installed on the device, the user will be prompted to
-         * install it inside the SupportMapFragment. This method will only be triggered once the
-         * user has installed Google Play services and returned to the app.
-         */
-        @Override
-        public void onMapReady(@NonNull GoogleMap googleMap) {
-            double lat = 0.0;
-            double lng = 0.0;
-            LatLng appointLoc = new LatLng(lat, lng);
 
-            // 移动地图到指定经度的位置
-            //googleMap.moveCamera(CameraUpdateFactory.newLatLng(appointLoc));
-
-            //添加标记到指定经纬度
-            googleMap.addMarker(new MarkerOptions().position(new LatLng(lat, lng)).title("Marker"));
-            //.icon(BitmapDescriptorFactory.fromResource(R.drawable.img_1)));
-            String[] locations = new String[] {"Newyork", "Beijing", "London"};
-            for(int i=0;i<locations.length;i++){
-                location=locations[i];
-                Geocoder geocoder = new Geocoder(getContext(), Locale.getDefault());
-                try {
-                    List<Address> listAddress = geocoder.getFromLocationName(location, 1);
-                    if (listAddress.size() > 0) {
-                        LatLng latLng = new LatLng(listAddress.get(0).getLatitude(), listAddress.get(0).getLongitude());
-                        googleMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
-                        googleMap.addMarker(new MarkerOptions().position(latLng).title("travel"));
-                    }
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-
-            googleMap.getUiSettings().setZoomControlsEnabled(true);
-
-            if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                // TODO: Consider calling
-                //    ActivityCompat#requestPermissions
-                // here to request the missing permissions, and then overriding
-                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                //                                          int[] grantResults)
-                // to handle the case where the user grants the permission. See the documentation
-                // for ActivityCompat#requestPermissions for more details.
-                ActivityCompat.requestPermissions(getActivity(),
-                        new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
-                        1);
-
-                ActivityCompat.requestPermissions(getActivity(),
-                        new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
-                        1);
-                return;
-            }
-            googleMap.setMyLocationEnabled(true);
-        }
-    };
 
     @Nullable
     @Override
@@ -101,10 +61,148 @@ public class ShowMapFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        SupportMapFragment mapFragment =
-                (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.showmap);
-        if (mapFragment != null) {
-            mapFragment.getMapAsync(callback);
+        smf = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.showmap);
+        client = LocationServices.getFusedLocationProviderClient(getContext());
+
+        Dexter.withContext(getContext())
+                .withPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+                .withListener(new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+                        getmylocation();
+                    }
+
+                    @Override
+                    public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+                        permissionToken.continuePermissionRequest();
+                    }
+                }).check();
+    }
+    public void getmylocation() {
+        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
         }
+        Task<Location> task = client.getLastLocation();
+        task.addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                smf.getMapAsync(new OnMapReadyCallback() {
+                    @Override
+                    public void onMapReady(@NonNull GoogleMap googleMap) {
+                        LatLng latlng=new LatLng(location.getLatitude(),location.getLongitude());
+                        googleMap.addMarker(new MarkerOptions().position(latlng).title("user"));
+                        googleMap.moveCamera(CameraUpdateFactory.newLatLng(latlng));
+
+                        Handler mHandler = new Handler(Looper.getMainLooper()) {
+                            @Override
+                            public void handleMessage(Message msg) {
+                                // 在主线程中更新 UI 界面
+                                List<String> locations= (List<String>)msg.obj;
+                                for(int i=0;i<locations.size();i++){
+                                    sLocation= locations.get(i);
+                                    Geocoder geocoder = new Geocoder(getContext(), Locale.getDefault());
+                                    try {
+                                        List<Address> listAddress = geocoder.getFromLocationName(sLocation, 1);
+                                        if (listAddress.size() > 0) {
+                                            LatLng latLng = new LatLng(listAddress.get(0).getLatitude(), listAddress.get(0).getLongitude());
+                                            //googleMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+                                            googleMap.addMarker(new MarkerOptions().position(latLng).title("travel"));
+                                        }
+                                    } catch (Exception e) {
+                                        e.printStackTrace();
+                                    }
+                                }
+                            }
+                        };
+
+                        new Thread(new Runnable() {
+                            @Override
+                            public void run() {
+                                // 在子线程中执行一些操作，并获取数据
+                                // 执行数据库操作
+                                DiaryDatabase db = DiaryDatabase.getInstance(getContext());
+                                List<String> locations = db.diaryDao().getLocation();
+
+                                // 将数据通过 Handler 传递给主线程
+                                Message msg = mHandler.obtainMessage();
+                                msg.obj = locations;
+                                mHandler.sendMessage(msg);
+                            }
+                        }).start();
+//                        Executor executor = Executors.newSingleThreadExecutor();
+//                        executor.execute(new Runnable() {
+//                            @Override
+//                            public void run() {
+//                                // 执行数据库操作
+//                                DiaryDatabase db = DiaryDatabase.getInstance(getContext());
+//                                List<String> locations = db.diaryDao().getLocation();
+//                                for(int i=0;i<locations.size();i++){
+//                                    sLocation= locations.get(i);
+//                                    Geocoder geocoder = new Geocoder(getContext(), Locale.getDefault());
+//                                    try {
+//                                        List<Address> listAddress = geocoder.getFromLocationName(sLocation, 1);
+//                                        if (listAddress.size() > 0) {
+//                                            LatLng latLng = new LatLng(listAddress.get(0).getLatitude(), listAddress.get(0).getLongitude());
+//                                            //googleMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+//                                            googleMap.addMarker(new MarkerOptions().position(latLng).title("travel"));
+//                                        }
+//                                    } catch (Exception e) {
+//                                        e.printStackTrace();
+//                                    }
+//                                }
+//                            }
+//                        });
+//                        String[] locations = new String[] {"Newyork", "Beijing", "London"};
+//                        for(int i=0;i<locations.length;i++){
+//                            sLocation=locations[i];
+//                            Geocoder geocoder = new Geocoder(getContext(), Locale.getDefault());
+//                            try {
+//                                List<Address> listAddress = geocoder.getFromLocationName(sLocation, 1);
+//                                if (listAddress.size() > 0) {
+//                                    LatLng latLng = new LatLng(listAddress.get(0).getLatitude(), listAddress.get(0).getLongitude());
+//                                    //googleMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+//                                    googleMap.addMarker(new MarkerOptions().position(latLng).title("travel"));
+//                                }
+//                            } catch (Exception e) {
+//                                e.printStackTrace();
+//                            }
+//                        }
+
+                        googleMap.getUiSettings().setZoomControlsEnabled(true);
+
+                        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                            // TODO: Consider calling
+                            //    ActivityCompat#requestPermissions
+                            // here to request the missing permissions, and then overriding
+                            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                            //                                          int[] grantResults)
+                            // to handle the case where the user grants the permission. See the documentation
+                            // for ActivityCompat#requestPermissions for more details.
+                            ActivityCompat.requestPermissions(getActivity(),
+                                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                                    1);
+
+                            ActivityCompat.requestPermissions(getActivity(),
+                                    new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                                    1);
+                            return;
+                        }
+                        googleMap.setMyLocationEnabled(true);
+                    }
+                });
+            }
+        });
     }
 }
